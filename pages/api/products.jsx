15075c1f8972167473556919babbb1139f76b3c8
Max-Bustamante69 <124266497+Max-Bustamante69@users.noreply.github.com>
2024-11-21 01:34:59 -0500

    git initial commit

---
+++ pages/api/products.jsx
@@ -0,0 +1,72 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Product } from '@/models/Product';
+import connectDB from '@/lib/mongoose';
+
+export default async function handler(req, res) {
+    const { method } = req;
+
+    if(method == 'POST'){
+        const { name, description, price } = req.body;
+        try {
+            await connectDB();
+            const product = await Product.create({ name, description, price });
+            res.status(201).json({ success: true, data: product });
+        } catch (error) {
+            res.status(400).json({ success: false });
+        }
+
+
+    }
+    else if(method == 'GET'){
+        try {
+            await connectDB();
+            if(req.query.id){
+                const product = await Product
+                    .findById(req.query.id)
+                res.status(200).json({ success: true, data: product });
+            }
+            else{
+                const products = await Product.find();
+                res.status(200).json({ success: true, data: products });
+            }
+        } catch (error) {
+            res.status(400).json({ success: false });
+        }
+    } else if (method == 'PUT'){
+        const { name, description, price } = req.body;
+        try {
+            await connectDB();
+            const product = await Product.findByIdAndUpdate(req.query.id, { name, description, price }, {
+                new: true,
+                runValidators: true
+            });
+            if(!product){
+                return res.status(400).json({ success: false });
+            }
+            res.status(200).json({ success: true, data: product });
+        }
+        catch (error) {
+            res.status(400).json({ success: false });
+        }
+        
+        
+    }
+    else if(method == 'DELETE'){
+
+        try {
+            await connectDB();
+            const product = await Product.findByIdAndDelete(req.query.id);
+            if(!product){
+                return res.status(400).json({ success: false });
+            }
+            res.status(200).json({ success: true, data: {} });
+        }
+        catch (error) {
+            res.status(400).json({ success: false });
+        }
+    }
+    else{
+        res.status(405).json({ success: false });
+    }
+
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#5542F6",
+        highlight: "#eae8fb",
+        bgGray: "#fbfafd",
       },
     },
   },
