5ec398c2bb49a4e52225e955db64b5f37def7df1
Max-Bustamante69 <124266497+Max-Bustamante69@users.noreply.github.com>
2025-02-16 23:44:53 -0500

    added dynamic fields selections in order to make products lighter to work with

--- ecommerce-frontend/src/app/actions/productActions.js
+++ ecommerce-frontend/src/app/actions/productActions.js
@@ -3,9 +3,18 @@ import connectDB from "@/app/lib/mongoose";
 import { Product } from "@/app/models/Product";
 import { getProductsByCategory } from "@/app/lib/fetchProducts";
 
-export async function getProductData(productId) {
+export async function getProductData(productId, fullProduct = false) {
   await connectDB();
-  const product = await Product.findById(productId);
+  const fieldsToSelect = fullProduct
+    ? {}
+    : {
+        variants: 0,
+        details: 0,
+        description: 0,
+        reference: 0,
+      };
+
+  const product = await Product.findById(productId).select(fieldsToSelect);
   const relatedProducts = await getProductsByCategory(
     product.category,
     4,
--- ecommerce-frontend/src/app/lib/fetchProducts.js
+++ ecommerce-frontend/src/app/lib/fetchProducts.js
@@ -4,38 +4,48 @@ import { Product } from "@/app/models/Product";
 import connectDB from "@/app/lib/mongoose";
 import serialize from "../utils/serializer";
 
-export async function getProductsByCategory(categoryId, limit = 6, filterId='') {
+export async function getProductsByCategory(
+  categoryId,
+  limit = 6,
+  filterId = "",
+  fullProduct = false
+) {
   await connectDB();
 
   try {
-    // 1️⃣ Find the parent category
     const parentCategory = await Category.findById(categoryId);
     if (!parentCategory) {
       console.error("❌ Category not found:", categoryId);
       return [];
     }
 
-    // 2️⃣ Get all subcategories that have this category as parent
     const subcategories = await Category.find({ parentCategory: categoryId });
-
-    // 3️⃣ Extract category IDs (parent + subcategories)
     const categoryIds = [
       new mongoose.Types.ObjectId(categoryId),
       ...subcategories.map((sub) => sub.id),
     ];
 
-    // 4️⃣ Query products that belong to any of these categories
+    // Select only necessary fields when not requesting full product
+    const fieldsToSelect = fullProduct
+      ? {}
+      : {
+          variants: 0,
+          details: 0,
+          description: 0,
+          reference: 0,
+        };
+
     const products = await Product.find({
       category: { $in: categoryIds },
-      ...(filterId && { _id: { $ne: filterId } })
-    }).limit(limit);
+      ...(filterId && { _id: { $ne: filterId } }),
+    })
+      .select(fieldsToSelect)
+      .limit(limit);
 
     products.forEach((product) => {
       product.img =
         "https://gratisography.com/wp-content/uploads/2024/11/gratisography-augmented-reality-800x525.jpg";
     });
-
-
 
     return serialize(products);
   } catch (error) {
@@ -43,5 +53,3 @@ export async function getProductsByCategory(categoryId, limit = 6, filterId='') 
     return [];
   }
 }
-
-
