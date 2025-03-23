5784e05076df42df396741d4b7774a71d05f6a5b
vale <vvhornung@eafit.edu.co>
2025-03-23 08:41:04 -0500

    Succees banner fix

---
+++ ecommerce-frontend/src/app/components/Success/styles/SuccessBanner.styled.js
@@ -0,0 +1,31 @@
+"use client"
+
+import styled from "styled-components";
+
+export const StyledSuccessBanner = styled.div`
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 50dvh;
+        text-align: center;
+        gap: 2rem;
+
+        a{
+            width: 100%;
+        }
+
+        button{
+            width: 20%;
+            padding: 0.5;
+            font-size: 0.8rem;
+            font-weight: 600;
+        }
+        
+            
+        
+
+
+
+    `;
