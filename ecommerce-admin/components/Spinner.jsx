828a37a02342c2c90ca3d61ad6e0a0b3b7b6b593
Max <124266497+Max-Bustamante69@users.noreply.github.com>
2025-05-23 18:59:06 -0500

    Delete ecommerce-admin directory

--- ecommerce-admin/components/Spinner.jsx
+++
@@ -1,5 +0,0 @@
-import { ClipLoader } from "react-spinners";
-
-export default function Spinner() {
-  return <ClipLoader color={"#000000"} speedMultiplier={2} />;
-}
--- ecommerce-admin/pages/api/orders.jsx
+++
@@ -1,8 +0,0 @@
-import connectDB from "@/lib/mongoose";
-import { Order } from "@/models/Order";
-
-
-export default async function handler(req, res){
-    await connectDB();
-    res.json(await Order.find().sort({ createdAt: -1 }));
-}
